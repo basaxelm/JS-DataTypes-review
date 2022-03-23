8fd208390b1384f7f7ac193b958c1b26319b2884
basaxelm <axelherbas.446@gmail.com>
2022-03-22 22:58:59 -0300

    Add some comments

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const string = "\'Love is the best thing in this world. Some found their love and some are still looking for their love.\'";
-let pattern = /love/gi;
+let pattern = /love/gi; // g specifies to search everywhere, i to be case-insensitive
 console.log(string.match(pattern)); //1
 
 const string2 = "\'You cannot end a sentence with because because because is a conjunction\'";
-let pattern2 = /because/gi;
+let pattern2 = /because/gi; 
 console.log(string2.match(pattern2)); //2
 
 let string3 = "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is #Project &is al@so $the $resu@lt of &love& of tea&ching";
-let pattern3 = /[%$@&#]/g;
+let pattern3 = /[%$@&#]/g; // a way to specify a pattern with a bunch of symbols
 console.log(string3.replace(pattern3, "")); //3
 
 const string4 = "\'He earns 5000 pesos from salary per month, 10000 pesos annual bonus, 15000 pesos online courses per month.\'";
-let pattern4 = /\d+/g;
+let pattern4 = /\d+/g; // d specifies to match digits, + one or more digits, g to search everywhere
 let income = string4.match(pattern4);
 let value1 = parseInt(income[0]);
 let value2 = parseInt(income[1]);
